f2a1671b58a9e05b04bb999eb1db32b85d07d9a5
Teodor Heggelund <git@teod.eu>
2026-02-03 20:19:14 +0100

    Establish publish machinery for new publish API contract
    Pushing a manifest isn't feasible due to security. We simply can't accept a
    manifest from anywhere.
    
    We can, however, accept a nudge that new docs are available, and a tentative doc
    list. Pushing a doc list merely updates the documents. And pushing a partial doc
    list doesn't update anything. I guess denial of service is a theoretical
    problem. But in that case, we hit Github rate limiting and ... move on with our
    life?

---
+++ publish.clj
@@ -0,0 +1,32 @@
+(ns publish
+  (:require [babashka.fs :as fs]
+            [babashka.http-client :as http-client]))
+
+(def ^{:dynamic true} *mikrobloggeriet-root*
+  "https://mikrobloggeriet.no")
+
+(defn load-cohort-docs [cohort]
+  (->> (fs/list-dir (:cohort/root cohort))
+       (keep (fn [doc-dir]
+               (when (and (fs/exists? (fs/file doc-dir "index.md"))
+                          (fs/exists? (fs/file doc-dir "meta.edn")))
+                 {:doc/slug (fs/file-name doc-dir)})))
+       (sort-by :doc/slug)))
+
+(defn manifest->request [{:as manifest :keys [cohorts]}]
+  {:method :post
+   :uri (str *mikrobloggeriet-root* "/fjernkohortene/docs")
+   :body (pr-str {:docs (mapcat load-cohort-docs cohorts)})})
+
+(comment
+
+  (def manifest (read-string (slurp "mikrobloggeriet.edn")))
+
+  ;; POST https://mikrobloggeriet.no/fjernkohortene/docs
+  (binding [*mikrobloggeriet-root* "http://localhost:7777"]
+    (http-client/request (manifest->request manifest)))
+
+  ;; POST http://localhost:7777/fjernkohortene/docs
+  (http-client/request (manifest->request manifest))
+
+  )
